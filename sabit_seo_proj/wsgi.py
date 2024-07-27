ebcdfdfcbec9a0bc4bd504d22fb61f04bea42316
abdul awal <awal@twintechsoft.com>
2024-07-27 11:26:35 +0600

    initial commit

---
+++ sabit_seo_proj/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for sabit_seo_proj project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sabit_seo_proj.settings')
+
+application = get_wsgi_application()
